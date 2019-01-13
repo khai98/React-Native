d6c629938f32d62cb392b3f4576d8cae794e9ed6
khai <hoangkhai1411111998@gmail.com>
2019-01-13 19:53:06 +0700

    code

---
+++ index.js
@@ -0,0 +1,14 @@
+/** @format */
+import {AppRegistry} from 'react-native';
+import {name as appName} from './app.json';
+// import native from './components/native'
+// import FlexDemension from './components/FlexDemension'
+// import Gretting from './components/Gretting'
+// import FixDemension from './components/FixDemension'
+// import FlexExample from './components/FlexExample'
+// import JustifyContent from './components/Justifycontent'
+// import ReactNativeInput from './components/ReactNativeInput'
+// import ReactNativeButton from './components/ReactNativeButton'
+import TouchableHighlight from './components/TouchableHighlight'
+
+AppRegistry.registerComponent(appName, () => TouchableHighlight);
